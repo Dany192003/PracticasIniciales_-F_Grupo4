3c8c1c8b2d9df2f10c2d56335e66ee1cfc064a98
Abel Enrique Paz Mendez <108552242+Xarbxa@users.noreply.github.com>
2024-03-13 22:56:21 -0600

    actualizando rutas

--- frontend/src/Rutas/Rutas.js
+++ frontend/src/Rutas/Rutas.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Login } from '../Componets/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RegistroUsuario } from '../Componets/RegistrarUsuario';
-import { TaskPage } from '../Componets/publicaciones';
+import { nuevaPublicacion } from '../Componets/publicacionFiltro';
 import { RecuperarContraseña } from '../Componets/recuContraseña';
 import { DatosUsuarios } from '../Componets/perfiles';
 
